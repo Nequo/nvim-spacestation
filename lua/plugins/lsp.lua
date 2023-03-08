dba900214c7f620105880ee2e262e3653478ff38
Nadim Edde Gomez <nadim@eddegomez.org>
2023-03-08 21:07:57 +0000

    feat(keybindings): add some descriptions

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,9 +7,9 @@ return {
             -- See `:help vim.diagnostic.*` for documentation on any of the below functions
             local opts = { noremap=true, silent=true }
             vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-            vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-            vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+            vim.keymap.set('n', '<space>dn', vim.diagnostic.goto_prev, {desc="prev diagnostic"})
+            vim.keymap.set('n', '<space>dp', vim.diagnostic.goto_next, {desc="next diagnostic"})
+            vim.keymap.set('n', '<space>dl', vim.diagnostic.setloclist, {desc="list diagnostic"})
 
             -- Use an on_attach function to only map the following keys
             -- after the language server attaches to the current buffer
@@ -25,16 +25,14 @@ return {
               vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
               vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
               vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-              vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-              vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
               vim.keymap.set('n', '<space>wl', function()
                 print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
               end, bufopts)
-              vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+              vim.keymap.set('n', '<space>l', vim.lsp.buf.type_definition, bufopts)
               vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
               vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
               vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-              vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+              vim.keymap.set('n', '<space>lf', function() vim.lsp.buf.format { async = true } end, bufopts)
             end
 
             local lsp_flags = {
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,13 +5,12 @@ return {
         'nvim-lua/plenary.nvim',
       },
       keys = {
-          {'<leader>ff', function() require('telescope.builtin').find_files() end},
-          {'<leader>fr', function() require('telescope.builtin').oldfiles() end},
-          {'<leader>fp', function() require('telescope.builtin').find_files({cwd = '~/.config/nvim/'}) end},
-          {'<leader>fg', function() require('telescope.builtin').live_grep() end},
-          {'<leader>fb', function() require('telescope.builtin').buffers() end},
-          {'<leader>fh', function() require('telescope.builtin').help_tags() end},
-          {'<leader>fa', function() require('telescope.builtin').lsp_code_actions() end},
+          {'<leader>ff', desc="find files", function() require('telescope.builtin').find_files() end},
+          {'<leader>fr', desc="recent files", function() require('telescope.builtin').oldfiles() end},
+          {'<leader>fp', desc="edit neovim config", function() require('telescope.builtin').find_files({cwd = '~/.config/nvim/'}) end},
+          {'<leader>fg', desc="live grep", function() require('telescope.builtin').live_grep() end},
+          {'<leader>fb', desc="find buffers", function() require('telescope.builtin').buffers() end},
+          {'<leader>fh', desc="help tags", function() require('telescope.builtin').help_tags() end},
       },
       opts = {
           defaults = {
