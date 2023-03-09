e2461d3e0471ddd48dd4f2122aea486a2c58c9c0
Nadim Edde Gomez <nadim@eddegomez.org>
2023-03-09 18:44:23 +0000

    feat(plugins): add highlight-colors

---
+++ lua/plugins/highlight-colors.lua
@@ -0,0 +1,8 @@
+return {
+    {
+        'brenoprata10/nvim-highlight-colors',
+        keys = {
+              {'<leader>tc', desc="toggle highlights", function() require("nvim-highlight-colors").toggle() end},
+        },
+    }
+}
