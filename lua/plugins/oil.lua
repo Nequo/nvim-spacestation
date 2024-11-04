990c158b9a3808f294d692bd98cb65c686ca305a
Nadim Edde Gomez <nadim@eddegomez.org>
2024-11-04 11:38:43 +0000

    feat(oil): new plugin

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,53 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      CustomOilBar = function()
+        local path = vim.fn.expand "%"
+        path = path:gsub("oil://", "")
+
+        return "  " .. vim.fn.fnamemodify(path, ":.")
+      end
+
+      require("oil").setup {
+        columns = {
+          "icon",
+          "permissions",
+          "size",
+          "mtime",
+        },
+        win_options = {
+          winbar = "%{v:lua.CustomOilBar()}",
+        },
+        view_options = {
+          show_hidden = true,
+        },
+        float = {
+          padding = 10,
+          max_width = 200,
+          max_height = 200,
+          border = "rounded",
+          win_options = {
+            winblend = 0,
+          },
+          -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+          get_win_title = nil,
+          -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+          preview_split = "auto",
+          -- This is the config that will be passed to nvim_open_win.
+          -- Change values here to customize the layout
+          override = function(conf)
+            return conf
+          end,
+        },
+      }
+
+      -- Open parent directory in current window
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+      -- Open parent directory in floating window
+      vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+    end,
+  },
+}
